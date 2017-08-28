e946dab8ad827c8416d93c2d659fc71deb77a554
unknown <rconnolly.rdk@outlook.com>
2017-08-27 20:52:22 -0400

    cleaned up tests

--- PaymentGateway.Tests/Tests.cs
+++ PaymentGateway.Tests/Tests.cs
@@ -11,14 +11,7 @@ namespace Tests
     public class Tests
     {
         [TestMethod]
-        public async Task TestMethod01()
-        {
-            var ret = await TestHttpMock01();
-            Assert.AreEqual(ret, "{'token' : '33300000-0000-0000-0000-000000000000'}");
-        }
-
-        [TestMethod]
-        public async Task TestMethod02()
+        public async Task When_Authorizing_Payment_Should_Return_Token()
         {
             var mockHttp = new MockHttpMessageHandler();
 
@@ -43,44 +36,7 @@ namespace Tests
         }
 
         [TestMethod]
-        public async Task IntegrationTest01()
-        {
-            // processor can change depending on provider fees per cart type, or processor outages.
-            // This can make it easy to switch providers easily with simple configurations.
-            var factory = new PaymentProcessorFactory<PaymentRequest, PaymentResponse>("mc");
-            var ccProcessor = factory.GetPaymentProcessor();
-
-            var paymentRequest = new PaymentRequest
-            {
-                AccountNumber = "1111222233334444"
-            };
-
-            var authResponse = await ccProcessor.AuthorizeAsync(new System.Net.Http.HttpClient(), paymentRequest);
-            var captResponse = await ccProcessor.CaptureAsync(new System.Net.Http.HttpClient(), paymentRequest);
-
-            // Console.WriteLine($"Auth Token: {authResponse.Token}");
-            // Console.WriteLine($"Capt Token: {captResponse.Token}");
-        }
-
-        private async Task<string> TestHttpMock01()
-        {
-            var mockHttp = new MockHttpMessageHandler();
-
-            // Setup a respond for the user api (including a wildcard in the URL)
-            mockHttp.When("http://localhost/api/gettoken/*").Respond("application/json", "{'token' : '33300000-0000-0000-0000-000000000000'}");
-
-            // Inject the handler or client into your application code
-            var client = mockHttp.ToHttpClient();
-
-            var response = await client.GetAsync("http://localhost/api/gettoken/1234");
-
-            var json = await response.Content.ReadAsStringAsync();
-
-            // No network connection required
-            return json;
-        }
-
-        private async Task<string> TestHttpMock02()
+        public async Task When_Capturing_Payment_Should_Return_Success_Equals_True()
         {
             var mockHttp = new MockHttpMessageHandler();
 
@@ -99,9 +55,33 @@ namespace Tests
                 AccountNumber = "1111222233334444"
             };
 
-            var response = await ccProcessor.AuthorizeAsync(mockHttp.ToHttpClient(), paymentRequest);
+            var response = await ccProcessor.CaptureAsync(mockHttp.ToHttpClient(), paymentRequest);
 
-            return response.Token;
+            Assert.AreEqual(response.Token, "33300000-0000-0000-0000-000000000000");
+        }
+
+        [TestMethod]
+        public async Task Integration_Test_For_Authorize_And_Capture()
+        {
+            // processor can change depending on provider fees per cart type, or processor outages.
+            // This can make it easy to switch providers easily with simple configurations.
+
+            var factory = new PaymentProcessorFactory<PaymentRequest, PaymentResponse>("mc");
+            var ccProcessor = factory.GetPaymentProcessor();
+
+            var paymentRequest = new PaymentRequest
+            {
+                AccountNumber = "1111222233334444"
+            };
+
+            var authResponse = await ccProcessor.AuthorizeAsync(new System.Net.Http.HttpClient(), paymentRequest);
+            var captResponse = await ccProcessor.CaptureAsync(new System.Net.Http.HttpClient(), paymentRequest);
+
+            // Console.WriteLine($"Auth Token: {authResponse.Token}");
+            // Console.WriteLine($"Capt Token: {captResponse.Token}");
+
+            Assert.AreEqual(authResponse.Token, "00000000-0000-0000-0000-000000000000");
+            Assert.AreEqual(captResponse.Token, "00000000-0000-0000-0000-000000000000");
         }
     }
 }
